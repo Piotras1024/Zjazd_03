05ef45eac774a020bd79ff0d9b7c3a5863b6a4d7
Piotr <piotr.w.dabrowski2@gmail.com>
2023-12-13 09:17:03 +0100

    change names

---
+++ czytanie_danych_z_pliku.py
@@ -0,0 +1,10 @@
+# przeczytać plik
+# wyczyścić dane
+# znaleźć liczbę słów
+# znaleźć liczbę różnych słów
+# zapisać ilosć powórzeń każdego słowa
+
+with open ('U2.txt', 'r') as u2:
+    content = u2.read()
+
+print(content)
