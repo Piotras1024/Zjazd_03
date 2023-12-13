59131664e239dad046ff4bb38cd0e73202f94523
Piotr <piotr.w.dabrowski2@gmail.com>
2023-12-13 15:46:34 +0100

    biblioteka datetime // zarzadzanie czasem

--- biblioteka_os_command_line_use.py
+++ biblioteka_os_command_line_use.py
@@ -9,3 +9,5 @@ os.system('cmd /c "ipconfig \all > results.txt"')
 
 
 os.system('cmd /c "cd C:\\Users\\Piotr\\Desktop && ipconfig \all > results.txt"') ## w 1 lini stworzy na pulpicie
+
+## > 1 strzałka nadpisuje !! ,>> 2 strzałki dopisują
