858870042d796c62aea05dd1faddce2858c84589
Piotr <piotr.w.dabrowski2@gmail.com>
2023-12-13 10:50:23 +0100

    reading_files_from_file, functions_for_reading_in_separeted_file

--- czytanie_danych_z_pliku.py
+++ czytanie_danych_z_pliku.py
@@ -4,7 +4,15 @@
 # znaleźć liczbę różnych słów
 # zapisać ilosć powórzeń każdego słowa
 
-with open ('U2.txt', 'r') as u2:
+with open('U2.txt', 'r') as u2:
     content = u2.read()
 
-print(content)
+
+from funkcje_do_czytanie_danych_z_pliku import *
+
+content = clear_text(content)
+content = make_split(content)
+
+print(counting_words_in_list(content))
+print(counting_unique_words_in_string(content))
+print(reps_of_word(content))
---
+++ funkcje_do_czytanie_danych_z_pliku.py
@@ -0,0 +1,29 @@
+def clear_text(text: str) -> str:
+    for sign in "()[]{},:;.'-":
+        text = text.replace(sign, "")
+    text = text.lower()
+    return text
+
+
+def make_split(text: str) -> list:
+    return text.split()
+
+
+def counting_words_in_list(list_of_words: list) -> int:
+    return len(list_of_words)
+
+
+def counting_unique_words_in_string(list_of_words: list) -> int:
+    return len(set(list_of_words))
+
+
+def reps_of_word(list_of_words: list) -> dict:
+    reps = {}
+    for word in list_of_words:
+        if word in reps.keys():
+            reps[word] += 1
+        else:
+            reps[word] = 1
+    reps = sorted(reps.items(), key=lambda x: x[1], reverse=True)
+
+    return reps
